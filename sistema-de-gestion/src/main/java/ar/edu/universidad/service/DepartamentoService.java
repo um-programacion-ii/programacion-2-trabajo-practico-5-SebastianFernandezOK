fe0cca1686cb2534d846606cbd2c7f9f9341bd67
SebastianFernandezOK <sebastianfernandez2812@gmail.com>
2025-08-24 17:26:22 -0300

    Etapa 2: Implementación de Repositories y Services

---
+++ sistema-de-gestion/src/main/java/ar/edu/universidad/service/DepartamentoService.java
@@ -0,0 +1,13 @@
+package ar.edu.universidad.service;
+
+import ar.edu.universidad.model.Departamento;
+import java.util.List;
+
+public interface DepartamentoService {
+    Departamento guardar(Departamento departamento);
+    Departamento buscarPorId(Long id);
+    Departamento buscarPorNombre(String nombre);
+    List<Departamento> obtenerTodos();
+    Departamento actualizar(Long id, Departamento departamento);
+    void eliminar(Long id);
+}
